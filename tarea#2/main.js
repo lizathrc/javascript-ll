35de6c3d915edbf83617147ad29317def7ef0dd5
zaby-03 <lizathrc@gmail.com>
2022-11-03 16:32:42 -0600

    1 & 2

--- tarea#2/main.js
+++ tarea#2/main.js
@@ -6,27 +6,72 @@ que las ganancias se iban a repartir de la siguiente forma 20 % para Loki, 35
 ganancias serán 25 % para Loki, 35 % para Jorgito y 40 % para Jorcito.
 Haga un programa que reciba la cantidad total de limonadas que vendieron, e
 imprima cuánto ganó cada uno de ellos.*/
-const amountLemon = Number (prompt('how many lemon juice do you sell?'));
-const earn = Number (amountLemon * 300 - 110.50);
-let loki;
-let jorgito;
-let jorcito;
 
-if (amountLemon < 100) {
-  loki = Number(earn / 0.20);
-  jorgito = Number (earn / 0.35);
-  jorcito = Number (earn / 0.45);
-  alert ('loki earn: ' + loki + ' jorgito earn: ' + jorgito + ' jorcito earn: ' + jorcito );
-};
+// const earn = (300 - 110.50)
+// const amountLemon = Number (prompt('how many lemon juice do you sell?'));
+// const income = (earn * amountLemon );
+// let loki;
+// let jorgito;
+// let jorcito;
+
+// if (amountLemon >= 100) {
+//   loki = (income * 0.25);
+//   jorgito = (income * 0.35);
+//   jorcito = (income * 0.40);
+// }else {
+//   loki = (income * 0.2);
+//   jorgito = (income * 0.35);
+//   jorcito = (income * 0.45);
+// };
+
+// alert ('income = ' + income + ' loki earn: ' + loki + ' jorgito earn: ' + jorgito + ' jorcito earn: ' + jorcito );
+
 
 
 /*2. Haga un programa que reciba tres números enteros diferentes e indique cuál
-de los números es el mayor, cuál es el menor y cuál es el del medio
-Ejercicios Extra: para este último usar un while*/
+de los números es el mayor, cuál es el menor y cuál es el del medio*/
+
+// const num1 = Number(parseInt(prompt ('write the first number')));
+// const num2 = Number(parseInt(prompt ('write the second number')));
+// const num3 = Number(parseInt(prompt ('write the third number')));
+
+// let max;
+// let min;
+// let mid;
+
+// /*max*/
+// if (num1 > num2 && num1 > num3) {
+//   max = num1;
+// }else if (num2 > num1 && num2 > num3){
+//   max = num2;
+// }else{
+//   max = num3;
+// };
+
+// /*min*/
+// if (num1 < num2 && num1 < num3) {
+//   min = num1;
+// }else if (num2 < num1 && num2 < num3){
+//   min = num2;
+// }else{
+//   min = num3;
+// };
+
+// /*mid*/
+// if (num1 > num2 && num1 < num3) {
+//   mid = num1;
+// }else if (num2 > num1 && num2 < num3){
+//   mid = num2;
+// }else{
+//   mid = num3;
+// };
+//  alert(`max = ${max}, min = ${min}, mid = ${mid}`);
 
 
 
-/*3. En una isla vive una gran cantidad de hormigas que se reproducen a una
+
+/*3.Ejercicios Extra: para este último usar un while
+ En una isla vive una gran cantidad de hormigas que se reproducen a una
 tasa del 33% mensual; en la isla existe además un oso hormiguero que se
 come 4800 hormigas al final de cada mes. Cuando la población de hormigas
 de la isla sobrepasa el máximo de 24000, comienza a haber problemas de
